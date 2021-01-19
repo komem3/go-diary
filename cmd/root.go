eaeb62d3f3ed7bcdbe3b7988a56ac942f38a47d7
komem3 <komata392@gmail.com>
2021-01-19 19:28:10 +0900

    update version no

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ func NewRootCommand(commands ...*cobra.Command) *cobra.Command {
 This application can create diary, format your diary directory, and make index file.
 `,
 		Args:    cobra.MinimumNArgs(1),
-		Version: "1.2.0",
+		Version: "1.4.1",
 	}
 	rootCmd.AddCommand(
 		commands...,
